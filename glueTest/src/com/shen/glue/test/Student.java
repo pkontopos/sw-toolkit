8872abf52724f0ee930d3d2a07ac307efa6a99cf
shenzhijie1979 <shenzhijie1979@gmail.com>
2010-12-03 02:24:54 +0000

    Incomplete - task : VBA development

--- glueTest/src/com/shen/glue/test/Student.java
+++ glueTest/src/com/shen/glue/test/Student.java
@@ -12,6 +12,8 @@ public class Student {
 	@GeneratedValue
 	Long id;
 	String name;
+	Integer score;
+	Boolean isSmart;
 
 	public Long getId() {
 		return id;
@@ -27,6 +29,22 @@ public class Student {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public Integer getScore() {
+		return score;
+	}
+
+	public void setScore(Integer score) {
+		this.score = score;
+	}
+
+	public Boolean getIsSmart() {
+		return isSmart;
+	}
+
+	public void setIsSmart(Boolean isSmart) {
+		this.isSmart = isSmart;
 	}
 
 	public Student() {
